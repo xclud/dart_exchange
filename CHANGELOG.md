d781d8ea73572349a55e34d96d98bb45815dd6b5
Mahdi K. Fard <mkfard@outlook.com>
2022-09-05 00:02:51 +0430

    Bump to version 0.0.2.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+* Adds basic functions.
+
 ## 0.0.1
 
 * Initial release.
--- lib/exchange.dart
+++ lib/exchange.dart
@@ -4,12 +4,17 @@ library exchange;
 import 'package:decimal/decimal.dart';
 
 abstract class Exchange {
-  Future<List<Token>> getTokens();
-  Stream<List<MarketDepth>> getMarketDepth(String from, String to);
+  const Exchange();
+
+  Stream<List<Market>> getMarketsStream();
+  Future<List<Asset>> getAssets();
+  Stream<List<PriceVolume>> getMarketDepth(Market market);
+  Stream<OrderBook> getMarketOrderBookStream(Market market);
+  Stream<MarketStat> getMarketStatStream(Market market);
 }
 
-class Token {
-  const Token({
+class Asset {
+  const Asset({
     required this.id,
     required this.name,
     required this.symbol,
@@ -20,12 +25,63 @@ class Token {
   final String symbol;
 }
 
-class MarketDepth {
-  const MarketDepth({
+class Market {
+  const Market({
+    required this.from,
+    required this.to,
+    this.stat,
+  });
+
+  final Asset from;
+  final Asset to;
+
+  final MarketStat? stat;
+}
+
+class PriceVolume {
+  const PriceVolume({
     required this.price,
     required this.volume,
   });
 
   final Decimal price;
   final Decimal volume;
+}
+
+class OrderBook {
+  const OrderBook({
+    required this.asks,
+    required this.bids,
+  });
+
+  final List<PriceVolume> asks;
+  final List<PriceVolume> bids;
+}
+
+class MarketStat {
+  const MarketStat({
+    required this.closed,
+    this.bestSell,
+    this.bestBuy,
+    this.volumeSrc,
+    this.volumeDst,
+    this.latest,
+    this.low,
+    this.high,
+    this.open,
+    this.close,
+    this.change,
+  });
+
+  final bool closed;
+  final Decimal? bestSell;
+  final Decimal? bestBuy;
+  final Decimal? volumeSrc;
+  final Decimal? volumeDst;
+  final Decimal? latest;
+  final Decimal? low;
+  final Decimal? high;
+  final Decimal? open;
+  final Decimal? close;
+  final Decimal? change;
 }
